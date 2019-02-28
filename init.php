b65526de1ad5f04e735aa2f95f877f73417f5e47
Gagan Deep Singh <me@gagan.pro>
2019-03-01 05:27:48 +0530

    Reformating the comment section on init file

--- init.php
+++ init.php
@@ -1,16 +1,17 @@
 <?php
-
-/*
-  Plugin Name: For Users Only
-  Description: Allows access to the site for logged in users only
-  Plugin URI: https://wordpress.org/plugins/for-users-only/
-  Version: 1.1
-  Author URI: https://gagan.pro
-  Author: Gagan Deep Singh
+/**
+ * Plugin Name: For Users Only
+ * Description: Allows access to the site for logged in users only
+ * Plugin URI: https://wordpress.org/plugins/for-users-only/
+ * Version: 1.1
+ * Author URI: https://gagan.pro
+ * Author: Gagan Deep Singh
+ *
+ * @package For_Users_Only
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	die; // Exit if accessed directly
+	die; // Exit if accessed directly.
 }
 
 require_once plugin_dir_path( __FILE__ ) . 'class-for-users-only.php';
